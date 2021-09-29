7de9b3a0af116ad9dfc4bfffea496e7e26035f54
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-09-29 21:49:52 +0100

    refactor BurgerBuilder to function class

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,26 +1,52 @@
-import React from 'react'
+import React , { useState }from 'react'
 import Burger from '../../components/Burger/Burger.js'
-
 import AuxHOC from '../../hoc/AuxHOC'
 
-class BurgerBuilder extends React.Component {
-  state = {
-    ingredients : {
-      salad: 1,
-      bacon: 1,
-      cheese: 1,
-      meat: 1
-    }
-  }
-  
-  render () {
+function BurgerBuilder () {
+  // state = {
+  //       ingredients : {
+  //         salad: 1,
+  //         bacon: 1,
+  //         cheese: 1,
+  //         meat: 1
+  //       }
+  //     }
+    const [meat, changeMeat] = useState(0);
+
     return (
       <AuxHOC>
-        <Burger ingredients={this.state.ingredients}/>
+        {/* <Burger ingredients={this.state.ingredients}/> */}
         <div>Build Controls</div>
+        <button type="button" onClick={() => changeMeat(meat+1)}>Click Me</button>
+        <h2>Meat's back on the menu bois {meat}</h2>
       </AuxHOC>
-    )
-  }
+    ) 
 }
+export default BurgerBuilder;
 
-export default BurgerBuilder
+// class BurgerBuilder extends React.Component {
+//   state = {
+//     ingredients : {
+//       salad: 1,
+//       bacon: 1,
+//       cheese: 1,
+//       meat: 1
+//     }
+//   }
+  
+
+//   render () {
+//     const [meat, changeMeat] = useState(0);
+
+//     return (
+//       <AuxHOC>
+//         <Burger ingredients={this.state.ingredients}/>
+//         <div>Build Controls</div>
+//         <button type="button" onClick={() => changeMeat(meat+1)}>Click Me</button>
+//         <h2>Meat's back on the menu bois {meat}</h2>
+//       </AuxHOC>
+//     ) 
+//   }
+// }
+
+// export default BurgerBuilder
