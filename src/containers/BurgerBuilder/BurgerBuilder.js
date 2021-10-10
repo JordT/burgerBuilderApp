e7bc581520b0f74447e87fa56acf86e68470b962
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-10-10 22:25:20 +0100

    Improved CSS on buttons

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -14,28 +14,36 @@ function BurgerBuilder () {
       <AuxHOC>
         <Burger meat={meat} salad={salad} bacon={bacon} cheese={cheese}/>
         <div>Build Controls</div>
-        <div> 
+        <div className={ classes.Container }> 
         {/* trying to get css to organise buttons */}
-          <button type="button" 
-            className={(meat <= 2) ? null : classes.ButtonDisabled } 
-            onClick={() => {(meat <= 2) ? changeMeat(meat+1) : console.log("That's enough meat.")}}>Add Meat</button>
-          <button type="button" 
-            onClick={() => {(meat !== 0) ? changeMeat(meat-1) : console.log("Meat cannot be negative")}}>Remove Meat</button>
-          <button type="button" 
-            className={(salad <= 2) ? null : classes.ButtonDisabled }
-            onClick={() => {(salad <= 2) ? changeSalad(salad+1) : console.log("That's enough salad.")}}>Add Salad</button>
-          <button type="button" 
-            onClick={() => {(salad !== 0) ? changeSalad(salad-1) : console.log("Salad cannot be negative")}}>Remove Salad</button>
-          <button type="button" 
-            className={(bacon <= 2) ? null : classes.ButtonDisabled }
-            onClick={() => {(bacon <= 2) ? changeBacon(bacon+1) : console.log("That's enough bacon.")}}>Add Bacon</button>
-          <button type="button" 
-            onClick={() => {(bacon !== 0) ? changeBacon(bacon-1) : console.log("Bacon cannot be negative")}}>Remove Bacon</button>
-          <button type="button" 
-            className={(cheese <= 2) ? null : classes.ButtonDisabled } 
-            onClick={() => {(cheese <= 2) ? changeCheese(cheese+1) : console.log("That's enough cheese.")}}>Add Cheese</button>
-          <button type="button" 
-            onClick={() => {(cheese !== 0) ? changeCheese(cheese-1) : console.log("Cheese cannot be negative")}}>Remove Cheese</button>
+          <div className={classes.Ingredient }>
+            <button type="button" 
+              className={(meat <= 2) ? null : classes.ButtonDisabled } 
+              onClick={() => {(meat <= 2) ? changeMeat(meat+1) : console.log("That's enough meat.")}}>Add Meat</button>
+            <button type="button" 
+              onClick={() => {(meat !== 0) ? changeMeat(meat-1) : console.log("Meat cannot be negative")}}>Remove Meat</button>
+          </div>
+          <div className={classes.Ingredient}>
+            <button type="button" 
+              className={(salad <= 2) ? null : classes.ButtonDisabled }
+              onClick={() => {(salad <= 2) ? changeSalad(salad+1) : console.log("That's enough salad.")}}>Add Salad</button>
+            <button type="button" 
+              onClick={() => {(salad !== 0) ? changeSalad(salad-1) : console.log("Salad cannot be negative")}}>Remove Salad</button>
+          </div>
+          <div className={classes.Ingredient}>
+            <button type="button" 
+              className={(bacon <= 2) ? null : classes.ButtonDisabled }
+              onClick={() => {(bacon <= 2) ? changeBacon(bacon+1) : console.log("That's enough bacon.")}}>Add Bacon</button>
+            <button type="button" 
+              onClick={() => {(bacon !== 0) ? changeBacon(bacon-1) : console.log("Bacon cannot be negative")}}>Remove Bacon</button>
+          </div>
+          <div className={classes.Ingredient}>
+            <button type="button" 
+              className={(cheese <= 2) ? null : classes.ButtonDisabled } 
+              onClick={() => {(cheese <= 2) ? changeCheese(cheese+1) : console.log("That's enough cheese.")}}>Add Cheese</button>
+            <button type="button" 
+              onClick={() => {(cheese !== 0) ? changeCheese(cheese-1) : console.log("Cheese cannot be negative")}}>Remove Cheese</button>
+          </div>
         </div>
       </AuxHOC>
     ) 
