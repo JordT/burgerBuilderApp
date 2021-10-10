64c4a47697c5ea9fb05154d3ea95e451136d9833
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-10-10 10:07:14 +0100

    tidied comments

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,21 +4,14 @@ import AuxHOC from '../../hoc/AuxHOC'
 
 
 function BurgerBuilder () {
-  // state = {
-  //       ingredients : {
-  //         salad: 1,
-  //         bacon: 1,
-  //         cheese: 1,
-  //         meat: 1
-  //       }
-  //     }
+
     const [meat, changeMeat] = useState(1);
     const [salad, changeSalad] = useState(2);
     const [bacon, changeBacon] = useState(1);
     const [cheese, changeCheese] = useState(1);
 
     const isZero = () => {
-    
+      // function to catch zero errors?
     }
     return (
       <AuxHOC>
@@ -36,30 +29,3 @@ function BurgerBuilder () {
     ) 
 }
 export default BurgerBuilder;
-
-// class BurgerBuilder extends React.Component {
-//   state = {
-//     ingredients : {
-//       salad: 1,
-//       bacon: 1,
-//       cheese: 1,
-//       meat: 1
-//     }
-//   }
-  
-
-//   render () {
-//     const [meat, changeMeat] = useState(0);
-
-//     return (
-//       <AuxHOC>
-//         <Burger ingredients={this.state.ingredients}/>
-//         <div>Build Controls</div>
-//         <button type="button" onClick={() => changeMeat(meat+1)}>Click Me</button>
-//         <h2>Meat's back on the menu bois {meat}</h2>
-//       </AuxHOC>
-//     ) 
-//   }
-// }
-
-// export default BurgerBuilder
