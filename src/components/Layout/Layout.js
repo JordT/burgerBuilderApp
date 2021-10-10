0113a3866c2779736a41ce00cecd56c426251be9
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-10-10 22:48:48 +0100

    added toolbar w/ css

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import AuxHOC from '../../hoc/AuxHOC'
+import Toolbar from '../Toolbar/Toolbar'
 import classes from '../Layout/Layout.module.css'
 
 const layout = (props) => (
   <AuxHOC>
+    <Toolbar />
     <div>Toolbar, SideDrawer, Backdrop</div>
     <main className={classes.Content}>
       {props.children}
