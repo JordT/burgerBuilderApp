f7e7c73839a0cfebf4966c38151cd767c3651f91
JordT <jordanthomson@Zoes-MacBook-Pro-2.local>
2021-10-12 23:16:12 +0100

    added styling to all control buttons

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -18,30 +18,34 @@ function BurgerBuilder () {
         <div className={classes.Controls}>Build Controls</div>
           <div className={classes.Ingredient }>
             <button type="button" 
-              className={(meat <= 2) ? null : classes.ButtonDisabled } 
+              className={(meat <= 2) ? classes.Button : classes.ButtonDisabled } 
               onClick={() => {(meat <= 2) ? changeMeat(meat+1) : console.log("That's enough meat.")}}>Add Meat</button>
             <button type="button" 
+              className={(meat !== 0) ? classes.Button : classes.ButtonDisabled } 
               onClick={() => {(meat !== 0) ? changeMeat(meat-1) : console.log("Meat cannot be negative")}}>Remove Meat</button>
           </div>
           <div className={classes.Ingredient}>
             <button type="button" 
-              className={(salad <= 2) ? null : classes.ButtonDisabled }
+              className={(salad <= 2) ? classes.Button : classes.ButtonDisabled }
               onClick={() => {(salad <= 2) ? changeSalad(salad+1) : console.log("That's enough salad.")}}>Add Salad</button>
             <button type="button" 
+              className={(salad !== 0) ? classes.Button : classes.ButtonDisabled } 
               onClick={() => {(salad !== 0) ? changeSalad(salad-1) : console.log("Salad cannot be negative")}}>Remove Salad</button>
           </div>
           <div className={classes.Ingredient}>
             <button type="button" 
-              className={(bacon <= 2) ? null : classes.ButtonDisabled }
+              className={(bacon <= 2) ? classes.Button : classes.ButtonDisabled }
               onClick={() => {(bacon <= 2) ? changeBacon(bacon+1) : console.log("That's enough bacon.")}}>Add Bacon</button>
             <button type="button" 
+              className={(bacon !== 0) ? classes.Button : classes.ButtonDisabled } 
               onClick={() => {(bacon !== 0) ? changeBacon(bacon-1) : console.log("Bacon cannot be negative")}}>Remove Bacon</button>
           </div>
           <div className={classes.Ingredient}>
             <button type="button" 
-              className={(cheese <= 2) ? null : classes.ButtonDisabled } 
+              className={(cheese <= 2) ? classes.Button : classes.ButtonDisabled } 
               onClick={() => {(cheese <= 2) ? changeCheese(cheese+1) : console.log("That's enough cheese.")}}>Add Cheese</button>
             <button type="button" 
+              className={(cheese !== 0) ? classes.Button : classes.ButtonDisabled } 
               onClick={() => {(cheese !== 0) ? changeCheese(cheese-1) : console.log("Cheese cannot be negative")}}>Remove Cheese</button>
           </div>
         </div>
